753bad4eb09a2addbc18d4935c7d7a43f73cedad
Will Berner <will.o.berner@gmail.com>
2024-05-14 18:37:12 -0400

    Initial commit - project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Components
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { EducationComponent } from './education/education.component';
+import { TeachingComponent } from './teaching/teaching.component';
+import { ResearchComponent } from './research/research.component';
+import { WorkComponent } from './work/work.component';
+
+// Routes, AKA pages on my website
+const homeRoute = {
+    path: '', component: HomeComponent
+}
+const aboutMeRoute = {
+    path: 'about', component: AboutMeComponent
+}
+const educationRoute = {
+    path: 'education', component: EducationComponent
+}
+const teachingRoute = {
+    path: 'teaching', component: TeachingComponent
+}
+const researchRoute = {
+    path: 'research', component: ResearchComponent
+}
+const workRoute = {
+    path: 'work', component: WorkComponent
+}
+
+export const routes: Routes = [
+    homeRoute, 
+    aboutMeRoute,
+    educationRoute,
+    teachingRoute,
+    researchRoute,
+    workRoute
+];
