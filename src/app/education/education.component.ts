e0096c85da7610532d420e57aa6a250e4ff9246d
Will Berner <will.o.berner@gmail.com>
2024-06-06 19:24:30 -0400

    Added coursework, working on education page

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 
 import text from '../descriptions.json'
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss'
 })
 export class EducationComponent {
 
-  public educationText:string = text.education;
+  public educationText = text.education;
 
 }
