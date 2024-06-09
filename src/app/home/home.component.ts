485ae152c502d3dff7c30cc44a61862594a65523
Will Berner <will.o.berner@gmail.com>
2024-06-09 13:53:50 -0400

    Updates to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 import text from '../descriptions.json'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-  public aboutMeText:string = text.home;
+  public aboutMeText = text.home;
 
 }
