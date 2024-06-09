43faaf94d5e5ea11f5f7f99f77db8829a695e44d
Will Berner <will.o.berner@gmail.com>
2024-06-09 14:51:12 -0400

    Started on feedback component

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 
 import text from '../descriptions.json'
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
 })
 export class FeedbackComponent {
 
-  public feedbackText:string = text.feedback;
+  public feedbackText= text.feedback;
 }
