a0df32e42c79058a714100eeba23242601106104
Will Berner <will.o.berner@gmail.com>
2026-05-31 00:46:56 -0400

    Freelance page info

--- src/app/freelance/freelance.component.ts
+++ src/app/freelance/freelance.component.ts
@@ -3,14 +3,6 @@ import { NgFor } from '@angular/common';
 
 import descriptions from '../descriptions.json';
 
-interface FreelanceProject {
-  name: string;
-  type: string;
-  description: string;
-  stack: string[];
-  url: string;
-}
-
 @Component({
   selector: 'app-work',
   standalone: true,
@@ -21,23 +13,5 @@ interface FreelanceProject {
 export class FreelanceComponent {
 
   public freelance = descriptions.freelance;
-
-  projects: FreelanceProject[] = [
-    {
-      name: 'Client Site One',
-      type: 'Small Business',
-      description: 'Brief description of what the site does and who it is for.',
-      stack: ['Angular', 'Bootstrap', 'SCSS'],
-      url: 'https://example.com'
-    },
-    {
-      name: 'Client Site Two',
-      type: 'Portfolio',
-      description: 'Brief description of what the site does and who it is for.',
-      stack: ['HTML', 'CSS', 'JavaScript'],
-      url: 'https://example.com'
-    },
-    // Add more projects here
-  ];
 
 }
