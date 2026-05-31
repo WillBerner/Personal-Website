e31fe2332d4d6bd80919d32b016e6c59a678ae97
Will Berner <will.o.berner@gmail.com>
2026-05-30 23:47:27 -0400

    Updating roles in work page

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
+import { NgForOf, NgClass } from '@angular/common';
 
 import text from '../descriptions.json'
 
 @Component({
   selector: 'app-work',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgForOf],
   templateUrl: './work.component.html',
   styleUrl: './work.component.scss'
 })
 export class WorkComponent {
 
   public workText= text.enterprise;
+  public roles = this.workText.descriptions.roles
 
 }
